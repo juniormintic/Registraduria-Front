7151f4672618080230babc3982c39874bd67621f
junior <jhgalvanmintic@gmail.com>
2022-11-16 21:34:35 -0500

    crear estructura de proyecto registraduria front

---
+++ src/app/pages/candidatos/candidatos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CandidatosRoutingModule } from './candidatos-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CandidatosRoutingModule
+  ]
+})
+export class CandidatosModule { }
