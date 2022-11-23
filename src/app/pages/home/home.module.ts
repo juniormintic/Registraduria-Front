1d69b783e9e97d209a96a9f00ffbe4f40f8b1681
juliana <julianamarinsalazar@gmail.com>
2022-11-22 20:10:03 -0500

    Home

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VistaprincipalComponent } from './vistaprincipal/vistaprincipal.component';
+
+
+
+@NgModule({
+  declarations: [
+    VistaprincipalComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
