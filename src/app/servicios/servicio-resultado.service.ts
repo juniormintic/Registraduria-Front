622b6164cb021d57a4eda1b023600e50f6ef94e4
junior <jhgalvanmintic@gmail.com>
2023-03-16 15:47:02 -0500

    pruebe de render 3

--- src/app/servicios/servicio-resultado.service.ts
+++ src/app/servicios/servicio-resultado.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Resultado } from '../modelos/resultado.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
